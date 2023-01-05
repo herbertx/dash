binpash/libdash

commit 46abc8c6d8a5e9a5712bdc1312c0b6960eec65a4

[BUILTIN] Add support for ulimit -r
I recently found myself in need to have dash support 'ulimit -r' to
set maximum realtime priority. Attached is a patch that adds the
parameter to the builtin ulimit command and updates the manpage.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/miscbltin.c
@@ -392,6 +392,9 @@ static const struct limits limits[] = {
 #ifdef RLIMIT_LOCKS
 	{ "locks",			RLIMIT_LOCKS,	   1, 'w' },
 #endif
+#ifdef RLIMIT_RTPRIO
+	{ "rtprio",			RLIMIT_RTPRIO,	   1, 'r' },
+#endif
 	{ (char *) 0,			0,		   0,  '\0' }
 };
 

src/dash.1
@@ -2163,6 +2163,8 @@ show or set the limit on the number of processes this user can
 have at one time
 .It Fl n
 show or set the limit on the number files a process can have open at once
+.It Fl r
+show or set the limit on the real-time scheduling priority of a process
 .El
 .Pp
 If none of these is specified, it is the limit on file size that is shown

herbertx/dash

commit 4bdefd16c6ea4b5b7c2b4dc2fccf5226401e13b7

builtin: Actually accept ulimit -r
The original commit that added it supposes this works, but it only adds
it to the ulimit -a listing and the manual, but doesn't allow it as an
option.

Fixes: 46abc8c6d8a5 ("[BUILTIN] Add support for ulimit -r")
Link: https://bugs.debian.org/975326
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/miscbltin.c
@@ -441,6 +441,9 @@ ulimitcmd(int argc, char **argv)
 #ifdef RLIMIT_LOCKS
 			       "w"
 #endif
+#ifdef RLIMIT_RTPRIO
+			       "r"
+#endif
 	)) != '\0')
 		switch (optc) {
 		case 'H':