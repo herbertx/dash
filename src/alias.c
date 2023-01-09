herbertx/dash

commit 4ec545e8dc98a3f461cf56bed03adafa81c64aec

alias: Quote name in printalias
This ensures even something like
  alias 'a|b|c=d'
is output by alias as
  'a|b|c'='d'
instead of
  a|b|c='d'
which is both "suitable for reinput to the shell" per POSIX
and doesn't execute the aliases as code.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/alias.c
@@ -197,7 +197,7 @@ freealias(struct alias *ap) {
 
 void
 printalias(const struct alias *ap) {
-	out1fmt("%s=%s\n", ap->name, single_quote(ap->val));
+	out1fmt("%s=%s\n", single_quote(ap->name), single_quote(ap->val));
 }
 
 STATIC struct alias **

commit b00288fd28c1b39d0f1531b6e6d86de59de4be8d

alias: fix name quoting in printalias
single_quote() over-writes the stack string, so just output the name
separately first.

Reported-by: Harald van Dijk <harald@gigawatt.nl>
Fixes: 4ec545e8dc98 ("alias: Quote name in printalias")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/alias.c
@@ -197,7 +197,8 @@ freealias(struct alias *ap) {
 
 void
 printalias(const struct alias *ap) {
-	out1fmt("%s=%s\n", single_quote(ap->name), single_quote(ap->val));
+	out1str(single_quote(ap->name));
+	out1fmt("=%s\n", single_quote(ap->val));
 }
 
 STATIC struct alias **