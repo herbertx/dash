herbertx/dash

commit 597850ae1fb9d70baf8dae7da0b5f5872654afba

shell: Use strcoll instead of strcmp where applicable
Use strcoll instead of strcmp so that the locale is taken into
account when sorting strings during pathname expansion, and for
the built-in test(1) string comparison operators.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/bltin/test.c
@@ -353,13 +353,13 @@ binop(void)
 		/* NOTREACHED */
 #endif
 	case STREQ:
-		return strcmp(opnd1, opnd2) == 0;
+		return strcoll(opnd1, opnd2) == 0;
 	case STRNE:
-		return strcmp(opnd1, opnd2) != 0;
+		return strcoll(opnd1, opnd2) != 0;
 	case STRLT:
-		return strcmp(opnd1, opnd2) < 0;
+		return strcoll(opnd1, opnd2) < 0;
 	case STRGT:
-		return strcmp(opnd1, opnd2) > 0;
+		return strcoll(opnd1, opnd2) > 0;
 	case INTEQ:
 		return getn(opnd1) == getn(opnd2);
 	case INTNE:

src/expand.c
@@ -1481,7 +1481,7 @@ msort(struct strlist *list, int len)
 	p = msort(p, len - half);		/* sort second half */
 	lpp = &list;
 	for (;;) {
-		if (strcmp(p->text, q->text) < 0) {
+		if (strcoll(p->text, q->text) < 0) {
 			*lpp = p;
 			lpp = &p->next;
 			if ((p = *lpp) == NULL) {

commit 6574d63a1163d8f97e2e9588bee80e0067080924

builtin: test = and != are strcmp, not strcoll
117067  s1 = s2  True if the strings s1 and s2 are identical; otherwise, false.
117068  s1 != s2 True if the strings s1 and s2 are not identical; otherwise, false.
117069  s1 > s2  True if s1 collates after s2 in the current locale; otherwise, false.
117070  s1 < s2  True if s1 collates before s2 in the current locale; otherwise, false.

"identical" does not mean "collate equally";
this is the difference between sort | uniq and sort -u

Fixes: 597850ae1fb9 ("shell: Use strcoll instead of strcmp where applicable")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/bltin/test.c
@@ -353,9 +353,9 @@ binop(void)
 		/* NOTREACHED */
 #endif
 	case STREQ:
-		return strcoll(opnd1, opnd2) == 0;
+		return strcmp(opnd1, opnd2) == 0;
 	case STRNE:
-		return strcoll(opnd1, opnd2) != 0;
+		return strcmp(opnd1, opnd2) != 0;
 	case STRLT:
 		return strcoll(opnd1, opnd2) < 0;
 	case STRGT: