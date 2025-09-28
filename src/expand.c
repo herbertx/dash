herbertx/dash

commit e878137f63e61926bbdff7062a558f0143ed5872

expand: Move stop array closer to strpbrk call
Move the stop array closer to the strpbrk(3) call in pmatch.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/expand.c
@@ -1886,9 +1886,6 @@ static __attribute__((noinline)) int ccmatch(char *p, const char *mbc, int ml,
 
 static int pmatch(char *pattern, const char *string)
 {
-    /* stop should be null-terminated as it passed as a string to
-     * strpbrk. */
-	char stop[] = { 0, CTLESC, CTLMBCHAR, '\0' };
 	const char *q;
 	unsigned mb;
 	char *p;
@@ -1918,11 +1915,17 @@ static int pmatch(char *pattern, const char *string)
 				c = *++p;
 			if (!c)
 				return 1;
-			stop[0] = CTLESC;
-			if (c != '?' && c != '[')
-				stop[0] = c;
+			if (c == '?' || c != '[')
+				c = CTLESC;
 			for (;;) {
-				if (stop[0] != (char)CTLESC) {
+				if (c != CTLESC) {
+					/* Stop should be null-terminated
+					 * as it is passed as a string to
+					 * strpbrk(3).
+					 */
+					const char stop[] = {
+						c, CTLESC, CTLMBCHAR, 0,
+					};
 					q = strpbrk(q, stop);
 					if (!q)
 						return 0;

commit 6dcc007a72f13c3e518a65bffef571795ad6678c

expand: Fix typo in pmatch for wildcard search
Skip the optimisation for * for only if the next character is
? and [, rather than ? and anything but [.

Reported-by: Reilly Brogan <reilly@reillybrogan.com>
Fixes: e878137f63e6 ("expand: Move stop array closer to strpbrk call")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/expand.c
@@ -1911,7 +1911,7 @@ static int pmatch(char *pattern, const char *string)
 				c = *++p;
 			if (!c)
 				return 1;
-			if (c == '?' || c != '[')
+			if (c == '?' || c == '[')
 				c = CTLESC;
 			for (;;) {
 				if (c != CTLESC) {