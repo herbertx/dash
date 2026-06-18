binpash/libdash

commit 6d8a11f801b41b629d0ad3c71fc5298354ae99ef

Eliminate first null termination in setvar.

src/var.c
@@ -231,9 +231,8 @@ setvar(const char *name, const char *val, int flags)
 	}
 	INTOFF;
 	p = mempcpy(nameeq = ckmalloc(namelen + vallen + 2), name, namelen);
-	*p++ = '\0';
 	if (val) {
-		p[-1] = '=';
+		*p++ = '=';
 		p = mempcpy(p, val, vallen);
 	}
 	*p = '\0';

herbertx/dash

commit f6098e65c318ec8baeb5722537d729569110dbe9

var: Restore double NUL on unset variables
This reverts commit 6d8a11f801b41b629d0ad3c71fc5298354ae99ef which
incorrectly removed the second NUL character for unset variables.

Without this NUL character the unset variables could become garbage
when used in dash internally (e.g., ps1val()).

Fixes: 6d8a11f801b4 ("Eliminate first null termination in setvar.")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/var.c
@@ -210,9 +210,9 @@ struct var *setvar(const char *name, const char *val, int flags)
 		vallen = strlen(val);
 	}
 	INTOFF;
-	p = mempcpy(nameeq = ckmalloc(namelen + vallen + 2), name, namelen);
+	p = mempcpy(nameeq = ckmalloc(namelen + vallen + 2), name, namelen + 1);
 	if (val) {
-		*p++ = '=';
+		p[-1] = '=';
 		p = mempcpy(p, val, vallen);
 	}
 	*p = '\0';