herbertx/dash

commit cb669294463ef454b59516016d91868a22d0eaef

shell: Call setlocale
Call setlocale to initialise locale settings for libc.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/main.c
@@ -32,6 +32,7 @@
  * SUCH DAMAGE.
  */
 
+#include <locale.h>
 #include <stdio.h>
 #include <signal.h>
 #include <sys/stat.h>
@@ -101,6 +102,9 @@ main(int argc, char **argv)
 #if PROFILE
 	monitor(4, etext, profile_buf, sizeof profile_buf, 50);
 #endif
+
+	setlocale(LC_ALL, "");
+
 	state = 0;
 	if (unlikely(setjmp(main_handler.loc))) {
 		int e;

commit 552c2b2f679d191a9de93cebcfb36581a2a87a4c

var: Call setlocale when LC variables are set
When any shell-relevant locale environment variable changes, call
putenv followed by setlocale so that the change is effective for
the current shell.

Reported-by: Vladimir Kudrya <vladimir.csp@gmail.com>
Fixes: cb669294463e ("shell: Call setlocale")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/main.c
@@ -32,7 +32,6 @@
  * SUCH DAMAGE.
  */
 
-#include <locale.h>
 #include <stdio.h>
 #include <signal.h>
 #include <sys/stat.h>
@@ -103,8 +102,6 @@ main(int argc, char **argv)
 #if PROFILE
 	monitor(4, etext, profile_buf, sizeof profile_buf, 50);
 #endif
-
-	setlocale(LC_ALL, "");
 
 	state = 0;
 	if (unlikely(setjmp(main_handler.loc))) {

src/var.c
@@ -32,6 +32,7 @@
  * SUCH DAMAGE.
  */
 
+#include <locale.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -81,6 +82,12 @@ MKINIT char defoptindvar[] = "OPTIND=1";
 int lineno;
 char linenovar[sizeof("LINENO=")+sizeof(int)*CHAR_BIT/3+1] = "LINENO=";
 
+static void changelocale(const char *val)
+{
+	putenv((char *)val);
+	setlocale(LC_ALL, nullstr);
+}
+
 /* Some macros in var.h depend on the order, add new variables to the end. */
 struct var varinit[] = {
 #if ATTY
@@ -101,6 +108,11 @@ struct var varinit[] = {
 	{ 0,	VSTRFIXED|VTEXTFIXED|VUNSET,	"TERM\0",	0 },
 	{ 0,	VSTRFIXED|VTEXTFIXED|VUNSET,	"HISTSIZE\0",	sethistsize },
 #endif
+	{ 0,	VSTRFIXED|VTEXTFIXED|VFULL|VUNSET, "LC_ALL\0",	changelocale },
+	{ 0,	VSTRFIXED|VTEXTFIXED|VFULL|VUNSET, "LC_COLLATE\0", changelocale },
+	{ 0,	VSTRFIXED|VTEXTFIXED|VFULL|VUNSET, "LC_CTYPE\0", changelocale },
+	{ 0,	VSTRFIXED|VTEXTFIXED|VFULL|VUNSET, "LC_NUMERIC\0", changelocale },
+	{ 0,	VSTRFIXED|VTEXTFIXED|VFULL|VUNSET, "LANG\0",	changelocale },
 };
 
 STATIC struct var *vartab[VTABSIZE];
@@ -158,6 +170,19 @@ static char *varnull(const char *s)
 {
 	/* Unset variables always end with two NUL chars. */
 	return strchrnul(s, '=') + 1;
+}
+
+static void varfunc(struct var *vp)
+{
+	const char *s;
+
+	if (!vp->func)
+		return;
+
+	s = vp->text;
+	if (!(vp->flags & VFULL))
+		s = varnull(s);
+	(*vp->func)(s);
 }
 
 /*
@@ -299,8 +324,8 @@ out_free:
 	vp->text = s;
 	vp->flags = flags;
 
-	if (vp->func && (flags & VNOFUNC) == 0)
-		(*vp->func)(varnull(s));
+	if (!(flags & VNOFUNC))
+		varfunc(vp);
 
 out:
 	return vp;
@@ -536,8 +561,8 @@ poplocalvars(void)
 				ckfree(vp->text);
 			vp->flags = lvp->flags;
 			vp->text = lvp->text;
-			if (vp->func && !(vp->flags & VNOFUNC))
-				(*vp->func)(varnull(vp->text));
+			if (!(vp->flags & VNOFUNC))
+				varfunc(vp);
 		}
 		ckfree(lvp);
 	}

src/var.h
@@ -48,7 +48,7 @@
 #define VSTACK		0x10	/* text is allocated on the stack */
 #define VUNSET		0x20	/* the variable is not set */
 #define VNOFUNC		0x40	/* don't call the callback function */
-/* #define VNOSET	0x80	   do not set variable - just readonly test */
+#define VFULL		0x80	/* pass value suitable for putenv */
 #define VNOSAVE		0x100	/* when text is on the heap before setvareq */
 
 